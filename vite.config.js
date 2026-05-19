80fd5a590bb04ecca8fbe10c773a3dcd5e5c98af
yunlei0707 <1051132042@qq.com>
2026-05-19 14:43:47 +0800

    fix: 加回external配置，确保APP能正常打开

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173
+  },
+  build: {
+    rollupOptions: {
+      external: ['@capacitor/core', '@capacitor/filesystem']
+    }
   }
 })
