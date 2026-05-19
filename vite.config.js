0b07ad6a1d7322d226749a4833e8bd7fb23cea40
yunlei0707 <1051132042@qq.com>
2026-05-19 13:34:52 +0800

    fix: 只移除vite external配置，修复版面混乱

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,5 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173
-  },
-  build: {
-    rollupOptions: {
-      external: ['@capacitor/core', '@capacitor/filesystem']
-    }
   }
 })
